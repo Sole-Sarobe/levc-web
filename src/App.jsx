0b683613c5b9186f9013dd5e51a7279bc441fc66
Soledad Sarobe <sarobesoledadprog@gmail.com>
2026-05-14 20:57:02 -0300

    vinculo page en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Inicio from "./pages/Inicio";
+import Productos from "./pages/Productos";
+import Herramientas from "./pages/Herramientas";
+import Pinturas from "./pages/Pinturas";
+import Contacto from "./pages/Contacto";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
+
         <Route path="/" element={<Inicio />} />
+
+        <Route
+          path="/productos"
+          element={<Productos />}
+        />
+
+        <Route
+          path="/herramientas"
+          element={<Herramientas />}
+        />
+
+        <Route
+          path="/pinturas"
+          element={<Pinturas />}
+        />
+
+        <Route
+          path="/contacto"
+          element={<Contacto />}
+        />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
