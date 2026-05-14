6ec0226c7966ba50c3b240c3e7b3acdc55a4a234
Soledad Sarobe <sarobesoledadprog@gmail.com>
2026-05-14 20:54:59 -0300

    vinculo page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import logo from "../assets/logo.png";
+
 import "./Header.css";
 
 function Header() {
@@ -21,11 +24,17 @@ function Header() {
       </div>
 
       <nav className={`nav ${menuOpen ? "active" : ""}`}>
-        <a href="#">Inicio</a>
-        <a href="#">Productos</a>
-        <a href="#">Herramientas</a>
-        <a href="#">Pinturas</a>
-        <a href="#">Contacto</a>
+
+        <Link to="/">Inicio</Link>
+
+        <Link to="/productos">Productos</Link>
+
+        <Link to="/herramientas">Herramientas</Link>
+
+        <Link to="/pinturas">Pinturas</Link>
+
+        <Link to="/contacto">Contacto</Link>
+
       </nav>
 
     </header>
